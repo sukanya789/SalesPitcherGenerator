923da98548d82348fc727a7216c65f8ee5d5eff3
SUKANYA SEMWAL <112698251+sukanya789@users.noreply.github.com>
2024-06-27 21:40:29 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+
+st.title("Sales Pitch and Email Content Generator")
+
+# Get user input
+url = st.text_input("Enter company URL:")
+api_key = st.text_input("Enter Gemini_api_key")
+if st.button("Generate"):
+    if url:
+        try:
+            response = requests.post("http://localhost:8000/generate_sales_pitch_and_email", data={"url": url, "api_key": api_key})
+            if response.status_code == 200:
+                html_content = response.content.decode("utf-8")
+                st.markdown(html_content, unsafe_allow_html=True)
+            else:
+                st.error(f"Error: {response.status_code} - {response.content.decode('utf-8')}")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Request failed: {e}")
+    else:
+        st.error("Please enter a valid URL")
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
